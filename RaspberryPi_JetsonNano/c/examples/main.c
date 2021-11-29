f2f4f94423250ec886d5ca03f605086d77face68
k27dong <me@kefan.me>
2021-11-29 13:34:19 -0500

    initial commit

---
+++ RaspberryPi_JetsonNano/c/examples/main.c
@@ -0,0 +1,29 @@
+﻿#include <stdlib.h>     //exit()
+#include <signal.h>     //signal()
+#include "EPD_Test.h"   //Examples
+
+void Handler (int signo) {
+  //System Exit
+  printf("\r\nHandler:exit\r\n");
+  DEV_Module_Exit();
+
+  exit(0);
+}
+
+int main(void) {
+  signal(SIGINT, Handler);
+
+  EPD_2in7_test();
+
+  // if(DEV_Module_Init()!=0){
+  //   return -1;
+  // }
+	// while(1) {
+  //   EPD_7in5b_V2_test();
+	// 	DEV_Delay_ms(10000);
+	// }
+	// DEV_Module_Exit();
+
+
+  return 0;
+}
